facd83d0ef282bf943bed21e58a0a5b70afb037b
Abdul Rehman <abdulrehmantahir142@gmail.com>
2026-01-27 18:52:04 +0500

    github pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-});
+  plugins: [react()],
+  base: "/portfoliowebsite/"
+})
